3d878166a128faabdb9ef48af3efcc59f5a8c002
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-06-19 19:17:20 +0900

    ➕Create

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,4 +1,5 @@
 class Api::BooksController < ApplicationController
+  protect_from_forgery :except => [:create]
   def index
     @books = Book.all
     render 'index', formats: :json, handlers: 'jbuilder'
@@ -6,5 +7,21 @@ class Api::BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     render 'show', formats: :json, handlers: 'jbuilder'
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      head :no_content
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.fetch(:book, {}).permit(
+      :title, :author, :publisher, :genre
+    )
   end
 end

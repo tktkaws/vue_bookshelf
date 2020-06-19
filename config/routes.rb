c5b06b869d50b93d31b8c3c27583e34db07439c7
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-06-19 19:26:33 +0900

    ➕ Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   namespace :api do
-    resources :books, only: [:show, :index, :create]
+    resources :books, only: [:show, :index, :create, :update]
   end
 end
